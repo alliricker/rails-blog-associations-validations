ad36f6b53aa096d89b15a2cd8e87d04dea67a455
Allison Ricker <alliricker@gmail.com>
2022-03-16 17:56:24 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
+    validates :name, presence: true
+    validates :content, presence: true
+
+    has_many :tags, through: :post_tags
+    has_many :post_tags
+    belongs_to :user
   
 end
